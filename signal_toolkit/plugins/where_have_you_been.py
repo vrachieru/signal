1ca15c37a78050a5f1a3220e67c4c6563b21e255
Victor Rachieru <victor.rachieru@gmail.com>
2019-09-10 16:40:57 +0300

    Persist device collected probes on set interval

--- signal_toolkit/plugins/where_have_you_been.py
+++ signal_toolkit/plugins/where_have_you_been.py
@@ -1,6 +1,8 @@
-import pyaml
+import yaml
 import scapy.all as scapy
 import sys
+import time
+import os
 
 from signal_toolkit.plugin import Plugin
 
@@ -11,13 +13,34 @@ class WhereHaveYouBeen(Plugin):
     for APs to which the device has been connected before.
     '''
 
-    def __init__(self):
+    def __init__(self, database='where_have_you_been.yaml', save_interval=60 * 5):
         self.devices = dict()
+        self.database = database
+
+        self.last_save = time.time()
+        self.save_interval = save_interval # seconds
 
     def process_packet(self, packet):
         if packet.raw_packet.haslayer(scapy.Dot11ProbeReq):
             if packet.ssid.strip():
-                self.devices[packet.src] = self.devices.get(packet.src, set()) | {packet.ssid}
+                self.devices[packet.src] = list(set(self.devices.get(packet.src, [])) | {packet.ssid})
+
+                now = time.time()
+                if now - self.last_save >= self.save_interval:
+                    self.last_save = now
+                    self.save()
+
+    def start(self):
+        self.load()
 
     def stop(self):
-        pyaml.dump(self.devices, sys.stdout)
+        self.save()
+
+    def load(self):
+        if os.path.isfile(self.database):
+            with open(self.database, 'r') as f:
+                self.devices = yaml.safe_load(f)
+
+    def save(self):
+        with open(self.database, 'w') as f:
+            yaml.dump(self.devices, f)
